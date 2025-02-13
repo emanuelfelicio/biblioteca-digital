fdcb1bc69009361d54c1e6ff1329cf8d4d51f6a8
Emanuel Felicio <emanuelfelicio10@gmail.com>
2025-02-13 12:11:46 -0300

    Adicionando o esqueleto do projeto

---
+++ crud.py
@@ -0,0 +1,11 @@
+def adicionar_livro():  # 1. Adicionar novos livros ao acervo
+    pass
+
+def listar_livros():  # 2. Listar e visualizar detalhes dos livros cadastrados
+    pass
+
+def editar_livro():  # 3. Editar informações de um livro existente
+    pass
+
+def remover_livro():  # 4. Remover um livro do acervo
+    pass
