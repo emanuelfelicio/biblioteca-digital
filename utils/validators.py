a262ead16d80f54d2d7db86e2a9f06168e7d11a7
Emanuel Felicio <emanuelfelicio10@gmail.com>
2025-02-21 19:01:50 -0300

    update-validators

--- utils/validators.py
+++ utils/validators.py
@@ -1,41 +1,55 @@
 from datetime import datetime
+from utils.ansi_colors import RED,RESET
 
 def validar_ano_publicacao(ano_publicacao):
 
     """
-    Valida se o ano de publicação está no formato correto e dentro do intervalo permitido.
+    Valida se o ano de publicação não está vazio, se está no formato correto e dentro do intervalo permitido.
 
     """
-    # Tenta converter para inteiro para validar o intervalo
-    try:
-        ano_int = int(ano_publicacao)
-    except ValueError:
-        raise ValueError("O ano de publicação deve conter apenas números.")
-
-    # Verifica se o ano está dentro do intervalo permitido
-    ano_atual = datetime.now().year  
-    if ano_int < 1400 or ano_int > ano_atual:
-        raise ValueError(f"O ano de publicação deve estar entre 1400 e {ano_atual}.")
-
     
+    # verfica se está vazio
+    if ano_publicacao:
+        try:
+            ano_int = int(ano_publicacao)
+        except ValueError:
+            raise ValueError(RED+"Erro: O ano de publicação deve conter apenas números."+RESET)
+        
+        # Verifica se o ano está dentro do intervalo permitido
+        ano_atual = datetime.now().year  
+        if ano_int < 1400 or ano_int > ano_atual:
+            raise ValueError(RED+f"Erro: O ano de publicação deve estar entre 1400 e {ano_atual}."+RESET)
+    else:
+        raise ValueError (RED+"Erro: O ano de publicação não pode estar vazio."+RESET)
+
     return ano_publicacao
 
 def validar_id_livro(id_livro):
 
     if not id_livro:  # verifica se id é vazio
-        raise ValueError("O id não pode ser vazio")
+        raise ValueError(RED+"Erro: O id não pode ser vazio"+RESET)
     
     try:
         id_int = int(id_livro)
     except ValueError:
-        raise ValueError("O id deve conter apenas números.")
+        raise ValueError(RED+"Erro: O id deve conter apenas números."+RESET)
     
     if id_int<0:
-        raise ValueError("O id não pode ser negativo.")
+        raise ValueError(RED+"Erro: O id não pode ser negativo."+RESET)
     
     return id_int
 
 def validar_campo_nao_vazio(value,nome_campo):
-    if not value:
-        raise ValueError(f"o campo {nome_campo} não pode estar vazio")
+     
+    """
+    Valida se o campo não está vazio.
+    Descrição:
+    Esta função verifica se o valor fornecido está vazio ou contém apenas espaços em branco.
+    Se o campo estiver vazio, solicita ao usuário que insira um valor válido até que um valor não vazio seja fornecido.
+
+    """
+    while not value or len(value.strip()) == 0:
+         print(RED + f"O campo {nome_campo} não pode estar vazio. Por favor, escreva novamente"+ RESET)
+         value = input(f"{nome_campo}: ")
     return value
+   
